55797cb6ae086125c82393e9982f93d97959ca03
itsmems <manishshrestha0803@gmail.com>
2023-05-02 00:17:37 +0545

    updated all

---
+++ src/Requests.js
@@ -0,0 +1,12 @@
+const key = '401dea9884f055ff9022d7695c0b7df1';
+
+const requests = {
+    requestPopular: `
+    https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
+    requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
+    requestTrending: `https://api.themoviedb.org/3/trending/all/day?api_key=${key}`,
+    // requestHorror: ``,
+    requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`,
+};
+
+export default requests;
